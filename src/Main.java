d02ffd0d3f0b80256c86de0a16ca4c07f7e57e34
Hamdi A Ahmed 12th CCHS <083829@SWCSD.US>
2024-01-25 13:43:22 -0500

    Initial commit

---
+++ src/Main.java
@@ -0,0 +1,15 @@
+// Press Shift twice to open the Search Everywhere dialog and type `show whitespaces`,
+// then press Enter. You can now see whitespace characters in your code.
+public class Main {
+    public static void main(String[] args) {
+        System.out.println("hello");
+        int[] weather = {1,2,3,4};
+        double avg = weather[0]+weather[1];
+//calculate avg
+        System.out.println(avg);
+
+
+        System.out.println(weather);
+
+    }
+}
